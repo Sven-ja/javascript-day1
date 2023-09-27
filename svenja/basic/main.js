229436c8c76f6725fb4bd1013dd2e1e1d45cf6a1
Svenja Schulmeister <schulmeister.svenja@gmail.com>
2023-09-27 11:39:11 +0200

    exercise 1-3

---
+++ svenja/basic/main.js
@@ -0,0 +1,8 @@
+document.write("Hello from the Browser");
+console.log("Hello from the Console");
+
+let myName = "Hi my name is Svenja";
+console.log(myName);
+
+let age = 32;
+console.log("and I am " + age + " years old.");
