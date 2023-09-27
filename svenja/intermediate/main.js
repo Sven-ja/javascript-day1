2569667b4bfbcd7d36fd176119458550dc696da9
Svenja Schulmeister <schulmeister.svenja@gmail.com>
2023-09-27 12:07:11 +0200

    added intermed-ex2

---
+++ svenja/intermediate/main.js
@@ -0,0 +1,23 @@
+// EXERCISE 1
+const cars = ["Tesla", "Audi", "Renault", "Volvo", "Mazda", "Fiat", "Ferrari"];
+console.log(cars);
+
+console.log(cars.sort());
+
+
+// EXERCISE 2
+const fruit = ["apple", "banana", "kiwi"];
+
+fruit.push("orange");
+console.log(fruit);
+
+fruit.pop();
+console.log(fruit);
+
+
+const animals = ["monkey", "horse", "dog"];
+animals.sort();
+console.log(animals);
+
+animals.unshift("cat");
+console.log(animals);
