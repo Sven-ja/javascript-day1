1cd6c8f327b1bad3c47c6023819676ebbb03f75d
Svenja Schulmeister <schulmeister.svenja@gmail.com>
2023-09-27 14:02:06 +0200

    edited sentence array

--- svenja/challenge/main.js
+++ svenja/challenge/main.js
@@ -1,5 +1,9 @@
 let sentenceUnstruc = "Properties,$a$set$of$immutable$values,$are$passed$to$a$component's$renderer$as$properties$in$its $HTML$tag.$A$component$cannot$directly$modify$any$properties$passed$to$it,$but$can$be$passed$ callback$functions$that$do$modify$values.$This$mechanism's$promise$is$expressed$as$properties $flow$down;$actions$flow$up";
 
-sentenceArray = sentenceUnstruc.split(/["$;"]/).join(" ");
+
+sentenceArray = sentenceUnstruc.split("$").join(" ");
+
+/* in order to split by multiply dividers:
+sentenceArray = sentenceUnstruc.split(/["$;"]/).join(" "); */
 
 console.log(sentenceArray);
