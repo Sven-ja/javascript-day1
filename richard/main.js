26d19c8b410b27a3de613055669c705eb175424c
Richard Waidacher <rw.tone@gmail.com>
2023-09-27 14:14:33 +0200

    intermediate work

--- richard/main.js
+++ richard/main.js
@@ -1,3 +1,5 @@
+// basic
+
 console.log("Hello");
 document.write("Hello");
 
@@ -13,3 +15,35 @@ let players = ["Martin", "Thomas", "Peter", "Mathias", "Niki"];
 console.log(players)
 
 console.log(`The most valuable player of the match is ${players[2]}.`)
+
+
+// intermediate 1
+
+const cars = ["Tesla", "Audi", "Renault", "Volvo", "Mazda", "Fiat", "Ferrari"];
+
+console.log(cars.sort());
+
+// intermediate 2
+
+const fruits = ["apple", "banana", "kiwi"]
+console.log(fruits);
+console.log(fruits.sort());
+fruits.push("orange");
+console.log(fruits);
+
+const removed = fruits.pop();
+console.log(removed);
+
+
+const pets = ["monkey", "horse", "dog"]
+console.log(pets);
+pets.unshift("cat");
+console.log(pets);
+
+// intermediate 3
+
+const fruit_Line = ["mango/cherries/kiwi/grapes/pear/peach/orange/lemon"]
+
+
+let fruit_sorted = fruit_Line.split("/");
+console.log(fruit_sorted);
